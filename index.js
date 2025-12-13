b04bc708a1410e061117494a1d14fa1fa31f7ecf
Shirgakiev David <david.shirgaliev@gmail.com>
2025-12-13 10:47:16 +0300

    Улучшил качество логотипа школы, добавил анимацию при наведение на кнопи шапки сайта добавил иконки на странице контакта школы

---
+++ index.js
@@ -0,0 +1,14 @@
+ReactDOM.render(React.createElement("",{
+  onMouseEnter: () => console.log("Mouse over"),
+  
+}), document.getElementById("katal"))
+
+import { Instagram } from 'lucide-react';
+
+const App = () => {
+  return (
+    <Instagram />
+  );
+};
+
+export default App;
